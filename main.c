51780f9561de7efa5216a017ace026c894d641a2
git noob <parkjoohyun12345@gmail.com>
2025-11-10 05:56:07 +0000

    fix code ver 1

--- main.c
+++ main.c
@@ -1,1 +1,9 @@
-hihihihi world
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main (void){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d %d\n",a,b);
+	printf("hello world");
+	return 0;
+}
