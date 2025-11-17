241e5107ecc11096f562704a3e78fa6b44e701c1
git noob <parkjoohyun12345@gmail.com>
2025-11-17 08:52:41 +0000

    fix code v3.5

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int plus(int a, int b);
-int main (void){
+int main (void)
 	int a,b;
-	scanf("%d %d",&a,&b);
-	printf("%d %d\n",a,b);
-	int res=plus(a,b);
-	printf("%d hello world",res);
-	printf("how do this");
-	printf("hello world");
+        scanf("%d %d",&a,&b);
+        printf("%d %d\n",a,b);
+        int res=plus(a,b);
+        printf("%d hello world",res);
+        printf("how do this");
+        printf("hello world");
 
 	return 0;
 }
 int plus(int a, int b){
-	res=a+b;
-	printf("loading...");
-	printf("done!");
+        res=a+b;
+        printf("loading...");
+        printf("done!");
 }
+
