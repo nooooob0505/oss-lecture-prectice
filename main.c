bba5ee8571893a290e151e93793fe3c0863c5ca3
git noob <parkjoohyun12345@gmail.com>
2025-11-17 08:30:04 +0000

    chekout test

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+int plus(int a,int b){
+	int p=a+b;
+	return p;
 int main (void){
 	int a,b;
 	scanf("%d %d",&a,&b);
 	printf("%d %d\n",a,b);
-	printf("hello world");
+	int res=plus(a,b);
+	printf("%d hello world",res);
+	printf("how do this");
+	print(res+a-b);
 	return 0;
+
 }
