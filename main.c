622e5c4ab186a06d8464887c95aacba2b6fce8eb
git noob <parkjoohyun12345@gmail.com>
2025-11-17 08:42:26 +0000

    try to merge to mater

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int plus(int a,int b){
-	int p=a+b;
-	return p;
+int plus(int a, int b);
 int main (void){
-	int a,b;
-	scanf("%d %d",&a,&b);
-	printf("%d %d\n",a,b);
-	int res=plus(a,b);
-	printf("%d hello world",res);
-	printf("how do this");
-	print(res+a-b);
-	return 0;
-
+        int a,b;
+        scanf("%d %d",&a,&b);
+        printf("%d %d\n",a,b);
+        int res=plus(a,b);
+        printf("%d hello world",res);
+        printf("how do this");
+        printf("hello world");
+        return 0;
 }
+int plus(int a, int b){
+        res=a+b;
+        printf("loading...");
+        printf("done!");
+}
+
